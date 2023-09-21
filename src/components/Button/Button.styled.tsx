1314d29932a3f992cb5443d84e9867b72b250683
Valya Danilova <danilova.vv@phystech.edu>
2023-09-21 06:57:43 +0400

    style:  button border change

--- src/components/Button/Button.styled.tsx
+++ src/components/Button/Button.styled.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { IButton } from '.';
 
 export const Button = styled.button<IButton>`
-  border: ${({ bordered }) => (bordered ? '1px solid #e9d9d8' : 'none')};
+  border: ${({ bordered }) => (bordered ? '1px solid #e9d9d8' : '1px solid transparent')};
   outline: none;
   font-size: 14px;
   font-weight: 300;
@@ -12,6 +12,7 @@ export const Button = styled.button<IButton>`
   color: #9b9b9b;
   cursor: pointer;
   border-radius: 4px;
+  box-sizing: border-box;
 
   &:hover {
     background: #f0f0f0;
