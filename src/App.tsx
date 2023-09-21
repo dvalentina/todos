d7c9c05065c476e0275bffa018dd7221c3182782
Valya Danilova <danilova.vv@phystech.edu>
2023-09-21 09:46:40 +0400

    refactor: move app logic from Card to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import AddToDo from './components/AddToDo';
 import Card from './components/Card';
+import Footer from './components/Footer';
+import ToDoComponent from './components/ToDoComponent';
 import { Container, Title } from './App.styled';
+import { generateId } from './utils';
+
+interface IToDo {
+  text: string;
+  done: boolean;
+  id: string;
+}
 
 function App() {
+  const [toDos, setToDos] = useState<IToDo[]>([
+    { text: 'Тестовое задание', done: false, id: generateId('Тестовое задание') },
+    { text: 'Прекрасный код', done: true, id: generateId('Прекрасный код') },
+    { text: 'Покрытие тестами', done: false, id: generateId('Покрытие тестами') },
+  ]);
+  const [toDoComponents, setToDoComponents] = useState<React.JSX.Element[]>([]);
+
+  const filterOptions = ['All', 'Active', 'Completed'];
+  const [chosen, setChosen] = useState(filterOptions[0]);
+
+  const addToDo = (text: string) => {
+    setToDos((prev) => [...prev, { text, done: false, id: generateId(text) }]);
+  };
+
+  const handleToDoClick = (id: string) => {
+    setToDos((prev) =>
+      prev.map((toDo) => {
+        if (toDo.id === id) {
+          return { ...toDo, done: !toDo.done };
+        }
+
+        return toDo;
+      }),
+    );
+  };
+
+  const handleChoose = (option: string) => {
+    setChosen(option);
+  };
+
+  useEffect(() => {
+    setToDoComponents(
+      toDos.map((toDo) => (
+        <ToDoComponent label={toDo.text} key={toDo.id} id={toDo.id} checked={toDo.done} handleClick={handleToDoClick} />
+      )),
+    );
+  }, [toDos]);
+
   return (
     <Container>
       <Title>todos</Title>
-      <Card />
+      <Card>
+        <AddToDo addToDo={addToDo} />
+        {toDoComponents}
+        <Footer
+          options={filterOptions}
+          chosen={chosen}
+          handleChoose={handleChoose}
+          handleClear={() => {
+            console.log('clear');
+          }}
+        />
+      </Card>
     </Container>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,71 +1,13 @@
-import React, { useEffect, useState } from 'react';
-
-import { generateId } from '../../utils';
-import AddToDo from '../AddToDo';
-import Footer from '../Footer';
-import ToDoComponent from '../ToDoComponent';
+import React from 'react';
 
 import { Card as StyledCard } from './Card.styled';
 
-interface IToDo {
-  text: string;
-  done: boolean;
-  id: string;
+interface ICard {
+  children?: React.ReactNode;
 }
 
-function Card() {
-  const [toDos, setToDos] = useState<IToDo[]>([
-    { text: 'Тестовое задание', done: false, id: generateId('Тестовое задание') },
-    { text: 'Прекрасный код', done: true, id: generateId('Прекрасный код') },
-    { text: 'Покрытие тестами', done: false, id: generateId('Покрытие тестами') },
-  ]);
-  const [toDoComponents, setToDoComponents] = useState<React.JSX.Element[]>([]);
-
-  const filterOptions = ['All', 'Active', 'Completed'];
-  const [chosen, setChosen] = useState(filterOptions[0]);
-
-  const addToDo = (text: string) => {
-    setToDos((prev) => [...prev, { text, done: false, id: generateId(text) }]);
-  };
-
-  const handleToDoClick = (id: string) => {
-    setToDos((prev) =>
-      prev.map((toDo) => {
-        if (toDo.id === id) {
-          return { ...toDo, done: !toDo.done };
-        }
-
-        return toDo;
-      }),
-    );
-  };
-
-  const handleChoose = (option: string) => {
-    setChosen(option);
-  };
-
-  useEffect(() => {
-    setToDoComponents(
-      toDos.map((toDo) => (
-        <ToDoComponent label={toDo.text} key={toDo.id} id={toDo.id} checked={toDo.done} handleClick={handleToDoClick} />
-      )),
-    );
-  }, [toDos]);
-
-  return (
-    <StyledCard>
-      <AddToDo addToDo={addToDo} />
-      {toDoComponents}
-      <Footer
-        options={filterOptions}
-        chosen={chosen}
-        handleChoose={handleChoose}
-        handleClear={() => {
-          console.log('clear');
-        }}
-      />
-    </StyledCard>
-  );
+function Card({ children }: ICard) {
+  return <StyledCard>{children}</StyledCard>;
 }
 
 export default Card;
