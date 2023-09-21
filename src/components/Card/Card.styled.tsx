35480af89d461458d03f8b3548df93350f2a7e91
Valya Danilova <danilova.vv@phystech.edu>
2023-09-21 10:08:09 +0400

    style: add decorative elements to card

--- src/components/Card/Card.styled.tsx
+++ src/components/Card/Card.styled.tsx
@@ -1,9 +1,44 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  background: #ffffff;
-  box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px,
-    rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;
+  background: white;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 2px 2px;
   min-height: 200px;
   width: 570px;
+  position: relative;
+
+  &::after {
+    content: '';
+    box-sizing: border-box;
+    width: 562px;
+    height: 5px;
+    position: absolute;
+    background: white;
+    bottom: -5px;
+    left: 4px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 2px 2px;
+    z-index: -1;
+  }
+
+  &::before {
+    content: '';
+    box-sizing: border-box;
+    width: 556px;
+    height: 10px;
+    position: absolute;
+    background: white;
+    bottom: -10px;
+    left: 7px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 2px 2px;
+    z-index: -2;
+  }
+`;
+
+export const Shadow = styled.div`
+  position: absolute;
+  top: 0;
+  height: -webkit-fill-available;
+  width: inherit;
+  z-index: -3;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 15px 29px 0px, rgba(0, 0, 0, 0.07) 0px 3px 4px 3px;
 `;
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import { Card as StyledCard } from './Card.styled';
+import { Card as StyledCard, Shadow } from './Card.styled';
 
 interface ICard {
   children?: React.ReactNode;
 }
 
 function Card({ children }: ICard) {
-  return <StyledCard>{children}</StyledCard>;
+  return (
+    <StyledCard>
+      {children}
+      <Shadow />
+    </StyledCard>
+  );
 }
 
 export default Card;
